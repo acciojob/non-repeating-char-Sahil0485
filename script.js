d386ff5fbe494c8ae64b505c4eec2176e468c2ff
SAHIL THAKUR <sahil20120067926thakur@gmail.com>
2025-08-22 22:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const frequencyMap = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    frequencyMap[char] = (frequencyMap[char] || 0) + 1;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (frequencyMap[str[i]] === 1) {
+      return str[i];
+    }
+  }
+
+  return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
